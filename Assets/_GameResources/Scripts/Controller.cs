728e7c0b1b71203666ec0660eca69694c775a1d0
Amjad <75468288+Amjad12-10@users.noreply.github.com>
2022-06-14 16:59:01 +0530

    Contols updated
    Satisfied with the result and yet the splash effect cannot find on internet

--- Assets/_GameResources/Scripts/Controller.cs
+++ Assets/_GameResources/Scripts/Controller.cs
@@ -1,6 +1,8 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
 
 public class Controller : MonoBehaviour
 {
@@ -8,9 +10,17 @@ public class Controller : MonoBehaviour
     [SerializeField] private float Rotationspeed = 2;
     [SerializeField] private float Controllerspeed = 5;
     [SerializeField] private GameObject ControllerChild;
+    // ------------------------------------- Game UI
+    [SerializeField] private Text Leveltext;
+    private int levelnumber;
     // ------------------------------------- Dead
     private Vector3 startposition = Vector3.zero;
     public bool isdead;
+    private void Start()
+    {
+        levelnumber = PlayerPrefs.GetInt("level");
+        Leveltext.text = "LEVEL - "+(levelnumber + 1);
+    }
 
     void Update()
     {
@@ -36,13 +46,21 @@ public class Controller : MonoBehaviour
         // --------------------------------- if dot,do not hit
         else
         {
-            var moveback = Vector3.Lerp(this.transform.position, startposition, Time.deltaTime * (Controllerspeed/2));
+            var moveback = Vector3.Lerp(this.transform.position, startposition, Time.deltaTime * (Controllerspeed/5));
             transform.position = moveback;
             ControllerChild.transform.Rotate(new Vector3(0, 0, -180) * Time.deltaTime * Rotationspeed);
-            if (transform.position == startposition)
+            if (transform.position == startposition || transform.position.y<1)
             {
                 isdead = false;
             }
         }
+        // --------------------------------- level complete
+        if (transform.position.y > 190)
+        {
+            levelnumber++;
+            PlayerPrefs.SetInt("level", levelnumber);
+            SceneManager.LoadScene(0);
+        }
     }
+    
 }
